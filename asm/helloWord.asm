e057487125419216697dc0d6592debf99a27cbee
Gurkamal Singh <gurkamalpsc@gmail.com>
2019-04-24 00:59:36 +0000

    mocked up a sample helloWorld.asm file - will run and test soon

--- asm/helloWord.asm
+++ asm/helloWord.asm
@@ -1,1 +1,15 @@
-# The MIPS version of Hello World!
+# Author:       Gurkamal Singh
+# Date:         April 23, 2019
+# Description:  The MIPS version of "Hello World!""
+
+                .data           # Data Declarations...load string into memory
+                                asdf
+output_string:  .asciiz         "\nHello, World!\n"
+
+                .text           # Instructions
+main:
+                li $v0, 4              # system call code for printing string
+                la $a0, output_string  # load address of string into $a0
+                syscall                # call OS to perform operation
+                li $v0, 10             # set up program termination call
+                syscall                # tell OS to do termination call
